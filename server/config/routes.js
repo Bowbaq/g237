0e63b2412f8e86473eb1ce1ec30b31805423915c
Maxime Bury <maxime.bury@gmail.com>
2012-12-06 14:07:33 -0500

    Set up the final routes for the API (I think :P)
    Signed-off-by: Maxime Bury <maxime.bury@gmail.com>

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,8 +5,17 @@ exports.routes = function (map) {
     api.post('authenticate', 'auth#authenticate');
     api.get('logout', 'auth#logout');
     
-    api.resources('projects', function(project){
-      project.resources('reviews');
+    api.resources('projects', { except: [ 'new', 'edit', 'destroy' ] }, function(project){
+      project.resources('reviews', { only: [ 'index', 'create', 'update', 'upvote', 'downvote' ] }, function(review) {
+        review.put('upvote', "reviews#upvote");
+        review.put('downvote', "reviews#downvote");
+      });
+      
+      project.post('team/:user_id', 'team#request');
+      project.put('team/:user_id', 'team#grant');
+      project.delete('team/:user_id', 'team#deny');
     });
+    
+    api.resources('users', { except: [ 'new', 'edit', 'destroy' ]});
   });
 };
