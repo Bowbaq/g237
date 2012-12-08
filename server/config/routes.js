7a5aaa49b2bb15abcfb8da48329424e4a6bc232d
Maxime Bury <maxime.bury@gmail.com>
2012-12-07 19:11:04 -0500

    Moved things around a bit on the server side (admin namespace)
    Signed-off-by: Maxime Bury <maxime.bury@gmail.com>

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,7 @@
 exports.routes = function (map) {
-  map.resources('users');
+  map.namespace('admin', function(admin){
+    admin.resources('users');
+  });
   
   map.namespace('api', function(api) {
     api.post('authenticate', 'auth#authenticate');
